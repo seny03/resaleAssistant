4d692ee56655727b415ea95847f5f4e1ae7a6ed7
caxapok1python <caxapok.js@yandex.ru>
2023-01-07 01:31:36 +0300

    logging not tested

--- markOutBot.py
+++ markOutBot.py
@@ -9,11 +9,15 @@ from offer_parser import Parser
 import time
 import validators
 import configparser
+import logging
+import logging.config
 
 # parse config
 conf = configparser.ConfigParser()
 conf.read('config.cfg')
 head_link = conf['parser']['link_head']
+logging.config.fileConfig(conf['log']['configfile'])
+logger = logging.getLogger('bot')
 
 # bot init
 bot = Bot(token=TOKEN)
@@ -27,14 +31,15 @@ startup_time = time.time()
 offers_from_user = {x: 0 for x in ADMIN_ID}
 
 
-async def warning(chat_id):
+async def warning(chat_id, username=None):
     await bot.send_message(chat_id, f"[!] Sorry, you don't have a permission to this bot. Your chat_id is {chat_id} !")
+    logger.warning(f"Logging attempt from wrong user chat_id={chat_id}, username={username}")
 
 
 async def add_offer(link, desired_price):
     info = parser.parse_link(link)
     db.add_offer(info, desired_price)
-    print(f"{info} {desired_price}")
+    logger.debug(f"ADD offer {info['id']}")
 
 
 async def send_answer(chat_id):
@@ -43,6 +48,7 @@ async def send_answer(chat_id):
     await asyncio.sleep(1)
     if offers_from_user[chat_id] == cur_offers_number:
         await bot.send_message(chat_id, f"[+] Successfully added {cur_offers_number} offers!")
+        logger.info("Successfully added {cur_offers_number} offers")
         offers_from_user[chat_id] = 0
 
 
@@ -52,29 +58,37 @@ async def start_command(message: types.Message):
     print(f'mes time: {(message.date.timestamp())}')
     chat_id = message.chat.id
     if chat_id not in ADMIN_ID:
-        return await warning(chat_id)
-    return await bot.send_message(chat_id, "[+] Successfully connected!")
+        return await warning(chat_id, message.chat.username)
+    await bot.send_message(chat_id, "[+] Successfully connected!")
+    logger.debug(f"Successful login attempt from chat_id={chat_id}, username={message.chat.username}")
 
 
 @dp.message_handler()
 async def get_message(message: types.Message):
     chat_id = message.chat.id
     if chat_id not in ADMIN_ID:
-        return await warning(chat_id)
+        return await warning(chat_id, message.chat.username)
     mes = message.text.split()
     if len(mes) != 2:
-        return await message.reply("[!] Incorrect arguments")
+        await message.reply("[!] Incorrect arguments")
+        logger.warning(f'Incorrect arguments chat_id={chat_id}')
+        return
     link, desired_price = mes
     desired_price = desired_price.replace(',', '.')
     if not validators.url(link) or head_link not in link:
-        return await message.reply("[!] Wrong link value")
+        await message.reply("[!] Wrong link value")
+        logger.warning(f'Wrong link value chat_id={chat_id}')
+        return
     if not desired_price.replace('.', '', 1).isdigit():
-        return await message.reply("[!] Wrong price value")
+        await message.reply("[!] Wrong price value")
+        logger.warning(f'Wrong price value chat_id={chat_id}')
+        return
 
     await add_offer(link, float(desired_price))
-    return await send_answer(chat_id)
+    await send_answer(chat_id)
+    return
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.DEBUG)
     executor.start_polling(dp)
